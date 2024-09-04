1c40f98925937340970e56e2848a5ec3e137d934
omarcoscardosobk <marcos.cardoso@zamp.com.br>
2024-09-04 17:24:52 -0300

    adicionado plugin accordeon do LaraZeus e melhorias na view lista

--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -1,5 +1,28 @@
-<!-- <div class="flex flex-col items-center p-2"> -->
 <div class="flex-col items-center p-2">
-<!-- <div>     -->
+    <x-zeus-accordion::accordion activeAccordion="3">
+
+        <x-zeus-accordion::accordion.item
+                :isIsolated="true"
+                :label="__('Inscritos')"
+                icon="heroicon-o-chevron-right"
+                badgeColor="danger"
+        >
+            <div class="bg-white p-4 *:py-2">
+                @livewire(\App\Filament\Widgets\InscritosOverview::class)        
+            </div>
+        </x-zeus-accordion::accordion.item>
+    
+        <x-zeus-accordion::accordion.item
+                :isIsolated="true"
+                :label="__('Pagamentos')"
+                icon="heroicon-o-chevron-right"
+        >
+            <div class="bg-white p-4 *:py-2">
+                @livewire(\App\Filament\Widgets\StatusPagamentosOverview::class)        
+            </div>
+        </x-zeus-accordion::accordion.item>
+    
+    </x-zeus-accordion::accordion>
+
     {{ $this->table }}
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './vendor/lara-zeus/accordion/resources/views/**/*.blade.php',
     ],
 }
