5d37fe018e8761bf174a0941fbf0365fc71d70e1
marcos.cardoso <marcos.cardoso@zamp.com.br>
2024-11-29 18:19:29 -0300

    Melhorias no form do ticket para celular

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-800 text-white top-0 left-0 right-0 z-100 flex flex-col items-center justify-center min-w-screen">
+<nav class="bg-gray-800 text-white top-0 left-0 right-0 z-100 flex flex-col items-center justify-center min-w-screen scale-110">
     <div class="container mx-auto px-10 py-2 flex justify-between items-center">
         <h1 class="text-2xl font-bold">e-Ticket</h1>
         <div class="flex items-center"> 
